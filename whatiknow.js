428ab630b62e1e4de6ac176059bd505727544d3d
levani-hacker <114279610+levani-hacker@users.noreply.github.com>
2023-10-07 18:08:17 +0400

    Add files via upload

---
+++ whatiknow.js
@@ -0,0 +1,60 @@
+// const numbers = [1, 2, 3, 4, 5];
+// for (const number of numbers) {
+//   console.log(number);
+// }
+
+
+
+
+// const person = {
+//     name: 'john',
+//     age: 30,
+//     city: 'kutaisi'
+//   };
+  
+//   for (const key in person) {
+//     console.log(`${key}: ${person[key]}`);
+//   }
+  
+
+
+
+
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [...arr1, 4, 5]; 
+
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { c: 3, ...obj1 }; 
+// console.log(obj2)
+
+
+
+
+// function greet(name = 'Guest') {
+//     console.log(`Hello, ${name}!`);
+//   }
+  
+//   greet();
+//   greet('Alice'); 
+              
+
+
+
+
+
+
+
+
+// const calculator = {
+//     add: function (a, b) {
+//       return a + b;
+//     },
+//     subtract: function (a, b) {
+//       return a - b;
+//     }
+//   };
+  
+//   console.log(calculator.add(5, 3));
+//   console.log(calculator.subtract(10, 2));
+  
